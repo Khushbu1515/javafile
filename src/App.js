a39999a476c9c72ae1dbe05ae336c984e2c0b31b
khushbu1515 <khushbu@matrixnmedia.com>
2023-08-22 16:07:23 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 
 
-import React from 'react';
-import Parent from "./Parent"
-
-
+import React , {useState} from 'react'
+import  Coll from "./Coll"
 
 const App = () =>{
    
+  const[data, setData]=useState("amaruu")
 
+  const Click =()=>
+  {
+    setData("amarnath sharma")
+  }
   return (
     <div>
-    <Parent/>
+    <h1>hello react</h1>
+    <Coll name={data}></Coll>
+    <button onClick ={Click}>update data</button>
     </div>)
 }
 export default App;
