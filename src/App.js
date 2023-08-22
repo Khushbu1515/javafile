0fb447c7f708f29ec488dfae9ccda2ab20f093c8
khushbu1515 <khushbu@matrixnmedia.com>
2023-08-22 18:56:45 +0530

    added a variable

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@
 import React , {useState} from 'react'
 import  Coll from "./Coll"
 
-const App = () =>{
+const App= () =>{
    
   const[data, setData]=useState("amaruu")
+   const dattype = "hello world"
 
-  const Click =()=>
-  {
-    setData("amarnath sharma")
-  }
   return (
     <div>
-    <h1>hello react</h1>
+    <h1>hello react {dattype}</h1>
     <Coll name={data}></Coll>
-    <button onClick ={Click}>update data</button>
+    <button onClick ={()=>{setData("amar nath sharma is java developer")}}>update data</button>
     </div>)
 }
 export default App;
