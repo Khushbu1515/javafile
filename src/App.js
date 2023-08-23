3a1c61272b895a1933a6fcc506420928a2fcfaf9
khushbu1515 <khushbu@matrixnmedia.com>
2023-08-23 18:34:06 +0530

    updated data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 
 
 import React from 'react';
-import {usestate} from "react";
+import {useState} from "react";
 
 const App = () =>{
    
-    const[data,setData]=usestate("khushbu");
+    const[data,setData]=useState("khushbu");
+    const handle=()=>
+    {
+      setData("khushbu kumari")
+    }
  
    return (
      <div>
      <hq  style={{color:"chocolate"}}>{data} </hq>
      <br/>
      <br/>
-     <button   style ={{color:'blueviolet'}}  onClick={()=>{setData("khushbu kumari")}}>update data</button>
+     <button   style ={{color:'blueviolet'}}  onClick={handle}>update data</button>
      </div>)
  }
  
