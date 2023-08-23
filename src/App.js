5a8da9ced738a8c15386e2627cd212cab314a8f5
khushbu1515 <khushbu@matrixnmedia.com>
2023-08-23 16:57:05 +0530

    update count

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 
 
 import React from 'react';
-import {useState}  from  "react"
+import {useState,useEffect}  from  "react"
 
 
 
 const App = () =>{
-   const[data,setData]=useState("khushbu");
+   const[count,setCount]=useState(0);
+   useEffect(()=>{
+    document.title=`you click {count} times`
+   },[count])
 
   return (
     <div>
-    <hq  style={{color:"chocolate"}}>{data} </hq>
+    <hq  style={{color:"chocolate"}}>{count} </hq>
     <br/>
     <br/>
-    <button   style ={{color:'blueviolet'}}  onClick={()=>{setData("khushbu kumari")}}>update data</button>
+    <button   style ={{color:'blueviolet'}}  onClick={()=>{setCount(count+1)}}>click {count} times</button>
     </div>)
 }
 export default App;
