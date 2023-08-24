f5e95c2347d41a3a7d488116d670e57b3c24ef5d
khushbu1515 <khushbu@matrixnmedia.com>
2023-08-24 20:06:17 +0530

    usecontext

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 
 
-import React, { useMemo, useState } from 'react';
+import React, { createContext, useContext } from 'react';
 
-const  App=()=> {
-    const [number, setNumber] = useState(0)
-    // Using useMemo
-    const squaredNum = useMemo(()=> {
-      return squareNum(number);
-    }, [number])
-    
-   
-   // Change the state to the input
-    const onChangeHandler = (e) => {
-      setNumber(e.target.value);
-    }
-     
-    return (
-      <div>
-       
-        <input type="number" placeholder="Enter a number"
-            value={number} onChange={onChangeHandler}>
-        </input>
-        <h1>output:{squaredNum}</h1>
-      </div>
-    );
-  }
-   
-  // function to square the value
-  function squareNum(number){
-    console.log("Squaring will be done!");
-    return Math.pow(number, 2);
-  }
-   
-  export default App;
+// Create a context
+const ThemeContext = createContext();
+
+// Create a component that provides the context value
+function ThemeProvider({ children }) {
+  const theme = 'light'; // You can replace this with dynamic logic
+  return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
+}
+
+// Create a component that consumes the context value using useContext
+function ThemeDisplay() {
+  const theme = useContext(ThemeContext);
+  return <div>Current theme: {theme}</div>;
+}
+
+// Your main app component
+function App() {
+  return (
+    <ThemeProvider>
+      <ThemeDisplay />
+    </ThemeProvider>
+  );
+}
+
+export default App;
